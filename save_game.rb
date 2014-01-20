401f57b662ecb543762e2b51e1cfb91aa3021f1d
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2014-01-20 19:23:11 +0000

    And now it even parses

--- save_game.rb
+++ save_game.rb
@@ -4,6 +4,12 @@ class SaveGame
   def initialize(path)
     @path = path
     @data = File.read(path).encode("UTF-8", "ISO-8859-1")
+    start_tokenizer!
+  end
+
+  def start_tokenizer!
+    @tokenizer = to_enum(:each_token)
+    @tokenbuf = []
   end
 
   def each_token
@@ -12,24 +18,83 @@ class SaveGame
     until s.eos?
       next if s.scan(/\s+/)
       if s.scan(/"(.*?)"/)
-        yield(:string, s[1])
-      elsif s.scan(/(\d+\.\d+\.\d+)/)
-        yield(:date, s[0])
+        yield([:value, s[1]])
+      elsif s.scan(/(\d+)\.(\d+)\.(\d+)/)
+        yield([:value, [:date, s[1], s[2], s[3]]])
       elsif s.scan(/(-?\d+\.\d+)/)
-        yield(:float, s[0])
+        yield([:value, s[0].to_f])
+      elsif s.scan(/\b(yes|no)\b/)
+        yield([:value, $1 == "yes"])
       elsif s.scan(/(-?\d+)/)
-        yield(:int, s[0])
+        yield([:value, s[0].to_i])
       elsif s.scan(/[a-zA-Z_\-][a-zA-Z_.0-9\-]*/)
-        yield(:symbol, s[0])
+        yield([:value, s[0].to_sym])
       elsif s.scan(/\{/)
-        yield(:start)
+        yield([:start])
       elsif s.scan(/\}/)
-        yield(:end)
-      elsif s.scan(/=/)
-        yield(:eq)
+        yield([:end])
+      elsif s.scan(/\=/)
+        yield([:eq])
       else
         raise "Parse error at position #{s.pos}}"
       end
+    end
+  end
+
+  def fetch_next_token
+    @tokenizer.next
+  rescue StopIteration
+    nil
+  end
+
+  def token(i)
+    return @tokenbuf[i] if @tokenbuf[i]
+    @tokenbuf << fetch_next_token while i >= @tokenbuf.size
+    @tokenbuf[i]
+  end
+
+  def token_type(i)
+    (token(i) || [])[0]
+  end
+
+  def shift(n=1)
+    n.times{ @tokenbuf.shift }
+  end
+
+  def eof?
+    !token(0)
+  end
+
+  def parse_error!
+raise "Parse error: #{token(0).inspect} #{token(1).inspect} #{token(2).inspect}..."
+  end
+
+  def parse_value
+    if token_type(0) == :value and token_type(1) == :eq
+      key = token(0)[1]
+      shift(2)
+      val = parse_value
+      {key => val}
+    elsif token_type(0) == :value
+      val = token(0)[1]
+      shift
+      val
+    elsif token_type(0) == :start
+      shift
+      val = []
+      while !eof? and token_type(0) != :end
+        val << parse_value
+      end
+      shift
+      val
+    else
+      parse_error!
+    end
+  end
+
+  def parse
+    until eof?
+      yield parse_value
     end
   end
 end
