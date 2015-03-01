ba14738a34f1df79840b5312a368479d8394e2da
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-03-01 16:01:51 +0000

    Silly typo, max_level not max_levels

--- fun_and_balance/fun_and_balance_common.rb
+++ fun_and_balance/fun_and_balance_common.rb
@@ -1031,31 +1031,31 @@ module FunAndBalanceCommon
     extra_custom_ideas = {
       "adm_tech_cost_modifier"             => [-0.05,  "adm"],
       "global_heretic_missionary_strength" => [ 0.01,  "adm"],
-      "migration_cooldown"                 => [-0.2,   "adm", max_levels: 3],
+      "migration_cooldown"                 => [-0.2,   "adm", max_level: 3],
       "years_of_nationalism"               => [-2,     "adm"],
       "monthly_fervor_increase"            => [ 0.25,  "adm"],
 
       "dip_tech_cost_modifier"             => [-0.05,  "dip"],
       "global_ship_cost"                   => [-0.05,  "dip"],
       "global_trade_goods_size"            => [ 0.05,  "dip"],
-      "envoy_travel_time"                  => [-0.25,  "dip", max_levels: 2],
+      "envoy_travel_time"                  => [-0.25,  "dip", max_level: 2],
       "caravan_power"                      => [ 0.1,   "dip"],
       "global_trade_income_modifier"       => [ 0.05,  "dip"],
       "improve_relation_modifier"          => [ 0.1,   "dip"],
-      "fabricate_claims_time"              => [-0.1,   "dip", max_levels: 6],
+      "fabricate_claims_time"              => [-0.1,   "dip", max_level: 6],
       "imperial_authority"                 => [ 0.05,  "dip"],
       "naval_attrition"                    => [-0.05,  "dip"],
       "relations_decay_of_me"              => [ 0.1,   "dip"],
       "papal_influence"                    => [ 0.1,   "dip"],
       "rebel_support_efficiency"           => [ 0.2,   "dip"],
       "trade_range_modifier"               => [ 0.1,   "dip"],
-      "justify_trade_conflict_time"        => [-0.1,   "dip", max_levels: 6],
+      "justify_trade_conflict_time"        => [-0.1,   "dip", max_level: 6],
       "global_own_trade_power"             => [ 0.1,   "dip"],
       "global_foreign_trade_power"         => [ 0.1,   "dip"],
-      "unjustified_demands"                => [-0.1,   "dip", max_levels: 6],
+      "unjustified_demands"                => [-0.1,   "dip", max_level: 6],
       "recover_navy_morale_speed"          => [ 0.025, "dip"],
-      "discovered_relations_impact"        => [-0.2,   "dip", max_levels: 3],
-      "global_ship_recruit_speed"          => [-0.1,   "dip", max_levels: 6],
+      "discovered_relations_impact"        => [-0.2,   "dip", max_level: 3],
+      "global_ship_recruit_speed"          => [-0.1,   "dip", max_level: 6],
       "province_warscore_cost"             => [-0.025, "dip"],
       "global_spy_defence"                 => [ 0.1,   "dip"],
       "global_ship_repair"                 => [ 0.1,   "dip"],
@@ -1065,7 +1065,7 @@ module FunAndBalanceCommon
       "global_garrison_growth"             => [ 0.1,   "mil"],
       "land_attrition"                     => [-0.05,  "mil"],
       "recover_army_morale_speed"          => [ 0.025, "mil"],
-      "global_regiment_recruit_speed"      => [-0.1,   "mil", max_levels: 6],
+      "global_regiment_recruit_speed"      => [-0.1,   "mil", max_level: 6],
       "free_leader_pool"                   => [ 1,     "mil", levels: [15, 50]]
     }
 
@@ -1092,28 +1092,28 @@ module FunAndBalanceCommon
           when [2, [1, 15], [2, 50]]
             idea.add! "level_cost_3", 105
             idea.add! "level_cost_4", 180
-            idea["max_levels"] = 4
+            idea["max_level"] = 4
           when [2, [1, 5], [2, 30]]
             idea.add! "level_cost_3", 75
             idea.add! "level_cost_4", 140
             idea.add! "level_cost_5", 225
-            idea["max_levels"] = 5
+            idea["max_level"] = 5
           when [2, [1, 3], [2, 18]]
             # 2n+1 * 60%, well, jusn following the formula
             idea.add! "level_cost_3", 45
             idea.add! "level_cost_4", 84
             idea.add! "level_cost_5", 135
-            idea["max_levels"] = 5
+            idea["max_level"] = 5
           when [2, [2, 15]]
             idea.add! "level_cost_3", 50
             idea.add! "level_cost_4", 105
             idea.add! "level_cost_5", 180
-            idea["max_levels"] = 5
+            idea["max_level"] = 5
           when [2, [1, 30], [2, 140]]
             # Extrapolating it level 3 would cost at level 12 which is out of scale
             # It's another issue if such high costs are really justified
           else
-            idea.add! "max_levels", 4 unless max_level
+            idea.add! "max_level", 4 unless max_level
             p [idea_name, max_level, *level_costs]
           end
         end
@@ -1123,11 +1123,11 @@ module FunAndBalanceCommon
           next unless cat == category
           options ||= {}
           idea = PropertyList[key, val]
-          if options[:max_levels]
-            idea.add! "max_levels", options[:max_levels]
+          if options[:max_level]
+            idea.add! "max_level", options[:max_level]
           end
           if options[:levels]
-            idea.add! "max_levels", options[:levels].size
+            idea.add! "max_level", options[:levels].size
             options[:levels].each_with_index do |cost, i|
               idea.add! "level_cost_#{i+1}", cost
             end
