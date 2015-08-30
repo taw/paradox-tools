706c6952d4fcdf88ddda7eef3c4418cb405fb882
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-08-30 10:45:22 +0100

    Refactored EU3 style elections out

--- fun_and_balance/fun_and_balance_common.rb
+++ fun_and_balance/fun_and_balance_common.rb
@@ -6,40 +6,11 @@ module FunAndBalanceCommon
   include FunAndBalanceFeatureHolySites
   include FunAndBalanceFeatureMissions
 
-
   def modify_node!(node, *modifications)
     modifications.each do |*path, expected, modified|
       actual = path.inject(node){|n,p| n ? n[p] : nil}
       raise "Node[#{path.join('/')}] expected `#{expected}', got `#{actual}'" unless actual == expected
       path[0..-2].inject(node){|n,p| n[p]}[path[-1]] = modified
-    end
-  end
-
-  def eu3_style_elections!(node)
-    node.delete("change_adm")
-    node.delete("change_dip")
-    node.delete("change_mil")
-    node["add_scaled_republican_tradition"] *= 0.3 if node["add_scaled_republican_tradition"]
-    node["define_ruler"].delete("fixed") if node["define_ruler"]
-    eu3_style_elections! node["FROM"] if node["FROM"]
-    eu3_style_elections! node["tooltip"] if node["tooltip"]
-  end
-
-  def change_elections_to_eu3_style!
-    patch_mod_file!("events/Elections.txt") do |node|
-      node.find_all("country_event").each do |event|
-        event.find_all("option").each do |option|
-          eu3_option = option.deep_copy
-          eu3_style_elections!(eu3_option)
-
-          next if option == eu3_option
-
-          eu3_option.add! "trigger", PropertyList[Property::NOT["has_global_flag", "fun_and_balance_config.disable_eu3_style_elections"]]
-          event.add! "option", eu3_option
-
-          option.add! "trigger", PropertyList["has_global_flag", "fun_and_balance_config.disable_eu3_style_elections"]
-        end
-      end
     end
   end
 
