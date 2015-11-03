dee020d59fe9bf1661bf64cb5747515aa8835975
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-11-03 00:23:00 +0000

    Peace of Westphalia is just a temporary arrangement now

--- ck2_mods/mods/protestantism.rb
+++ ck2_mods/mods/protestantism.rb
@@ -22,16 +22,16 @@ class ProtestantismGameModification < CK2GameModification
   end
 
   def setup_protestantism!
-    %W[religion_icon_strip.dds religion_icon_strip_small.dds religion_icon_strip_big.dds].each do |name|
-      create_file! "gfx/interface/#{name}", open("data/modern_times/#{name}", "rb", &:read)
-    end
-    generalstuff = ParadoxModFile.new(path: "data/vanilla/generalstuff.gfx").parse!
-    generalstuff["spriteTypes"].find_all("spriteType").each do |data|
-      next unless data["name"] =~ /\AGFX_religion_icon_strip/
-      raise unless data["noOfFrames"] = 52
-      data["noOfFrames"] = 54
-    end
-    create_mod_file! "interface/generalstuff.gfx", generalstuff
+    # %W[religion_icon_strip.dds religion_icon_strip_small.dds religion_icon_strip_big.dds].each do |name|
+    #   create_file! "gfx/interface/#{name}", open("data/modern_times/#{name}", "rb", &:read)
+    # end
+    # generalstuff = ParadoxModFile.new(path: "data/vanilla/generalstuff.gfx").parse!
+    # generalstuff["spriteTypes"].find_all("spriteType").each do |data|
+    #   next unless data["name"] =~ /\AGFX_religion_icon_strip/
+    #   raise unless data["noOfFrames"] = 52
+    #   data["noOfFrames"] = 54
+    # end
+    # create_mod_file! "interface/generalstuff.gfx", generalstuff
 
     create_mod_file! "history/titles/d_protestant.txt", PropertyList[
       Date.parse("0020.1.1"), PropertyList["active", false],
@@ -57,9 +57,10 @@ class ProtestantismGameModification < CK2GameModification
       "christian", PropertyList[
         "protestant", PropertyList[
           "graphical_culture", "westerngfx",
-          "icon", 53,
-          "heresy_icon", 54,
+          "icon", 1,
+          "heresy_icon", 6,
           "color", [0.7, 0.7, 0.8],
+          "parent", "catholic",
           "crusade_name", "CRUSADE",
           "scripture_name", "THE_BIBLE",
           "priest_title", "PRIEST",
@@ -76,9 +77,10 @@ class ProtestantismGameModification < CK2GameModification
         ],
         "reformed", PropertyList[
           "graphical_culture", "westerngfx",
-          "icon", 53,
-          "heresy_icon", 54,
+          "icon", 1,
+          "heresy_icon", 6,
           "color", [0.4, 0.4, 0.6],
+          "parent", "catholic",
           "crusade_name", "CRUSADE",
           "scripture_name", "THE_BIBLE",
           "priest_title", "PRIEST",
@@ -91,7 +93,6 @@ class ProtestantismGameModification < CK2GameModification
           "priests_can_marry", true,
           "religious_clothing_head", 0,
           "religious_clothing_priest", 1,
-          "parent", "protestant",
         ],
       ],
     ]
@@ -113,7 +114,90 @@ class ProtestantismGameModification < CK2GameModification
     )
   end
 
+  def setup_peace_of_westphalia_cbs!
+    patch_mod_file!("common/cb_types/00_cb_types.txt") do |node|
+      ["religious", "crusade"].each do |cb_name|
+        node[cb_name]["can_use"].add! "OR", PropertyList[
+          "has_global_flag", "end_of_peace_of_westphalia",
+          "FROM", PropertyList[
+            "NOT", PropertyList["religion", "catholic"],
+            "NOT", PropertyList["religion", "protestant"],
+            "NOT", PropertyList["religion", "reformed"],
+          ],
+          "ROOT", PropertyList[
+            "NOT", PropertyList["religion", "catholic"],
+            "NOT", PropertyList["religion", "protestant"],
+            "NOT", PropertyList["religion", "reformed"],
+          ],
+        ]
+      end
+    end
+  end
+
+  def setup_peace_of_westphalia!
+    decisions = []
+    decisions << Property[
+      "end_peace_of_westphalia", PropertyList[
+        "is_high_prio", true,
+        "potential", PropertyList[
+          "OR", PropertyList[
+            "religion", "catholic",
+            "religion", "protestant",
+            "religion", "reformed",
+          ],
+          "NOT", PropertyList["has_global_flag", "end_of_peace_of_westphalia"],
+        ],
+        "allow", PropertyList[
+          "independent", true,
+          "religion_authority", 1.0,
+          "prestige", 8000,
+        ],
+        "effect", PropertyList[
+          "set_global_flag", "end_peace_of_westphalia",
+        ],
+        "revoke_allowed", PropertyList["always", false],
+        "ai_will_do", PropertyList["factor", 1],
+      ],
+    ]
+    # decisions << Property[
+    #   "end_peace_of_westphalia_debug", PropertyList[
+    #     "is_high_prio", true,
+    #     "allow", PropertyList[
+    #       "ai", false,
+    #       "NOT", PropertyList["has_global_flag", "end_of_peace_of_westphalia"],
+    #     ],
+    #     "effect", PropertyList[
+    #       "set_global_flag", "end_of_peace_of_westphalia",
+    #     ],
+    #     "revoke_allowed", PropertyList["always", false],
+    #     "ai_will_do", PropertyList["factor", 1],
+    #   ],
+    # ]
+    # decisions << Property[
+    #   "resume_peace_of_westphalia_debug", PropertyList[
+    #     "is_high_prio", true,
+    #     "allow", PropertyList[
+    #       "ai", false,
+    #       "has_global_flag", "end_of_peace_of_westphalia",
+    #     ],
+    #     "effect", PropertyList[
+    #       "clr_global_flag", "end_of_peace_of_westphalia",
+    #     ],
+    #     "revoke_allowed", PropertyList["always", false],
+    #     "ai_will_do", PropertyList["factor", 1],
+    #   ],
+    # ]
+    create_mod_file! "decisions/peace_of_westphalia.txt", PropertyList[
+      "decisions", PropertyList[*decisions],
+    ]
+    localization! "modern_times_peace_of_westphalia",
+      "end_peace_of_westphalia" => "Renounce Peace of Westphalia",
+      "end_peace_of_westphalia_desc" => "Uneasy truce between Catholic, Protestant, and Reformed branches of Christianity held since 1648. It is time to renounce it and unite Western Christianity!"
+  end
+
   def apply!
     setup_protestantism!
+    setup_peace_of_westphalia_cbs!
+    setup_peace_of_westphalia!
   end
 end
