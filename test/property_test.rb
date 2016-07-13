beeceef91a4ae5532f01936800bebc3f9dfe3aee
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-07-13 01:47:26 +0100

    A bit unsure abuot eql? in this context

--- test/property_test.rb
+++ test/property_test.rb
@@ -14,4 +14,30 @@ class PropeltyTest < MiniTest::Test
     assert_equal Property["AND", PropertyList["foo", "bar", "hello", "world"]], Property::AND["foo", "bar", "hello", "world"]
     assert_equal Property["OR", PropertyList["foo", "bar", "hello", "world"]], Property::OR["foo", "bar", "hello", "world"]
   end
+
+  def test_hash
+    a = Property["foo", 1]
+    b = Property["foo", 1]
+    c = Property["foo", 2]
+    d = Property["bar", 1]
+    e = Property["foo", 1.0]
+    h = {a => 123, c => 456}
+    assert_equal 123, h[a]
+    assert_equal 123, h[b]
+    assert_equal 456, h[c]
+    assert_equal nil, h[d]
+    assert_equal nil, h[e]
+  end
+
+  def test_special_value_hash
+    a = Property["foo", Property::GT[1]]
+    b = Property["foo", Property::LT[1]]
+    c = Property["foo", 1]
+    d = Property["foo", Property::GT[1]]
+    h = {a => 123, b => 456, c => 789}
+    assert_equal 123, h[a]
+    assert_equal 456, h[b]
+    assert_equal 789, h[c]
+    assert_equal 123, h[d]
+  end
 end
