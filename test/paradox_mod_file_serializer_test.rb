e77d81818d0acee6cffc88b255354822af82c8dc
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-07-21 22:42:06 +0100

    Extra tests for ParadoxModFileSerializer

--- test/paradox_mod_file_serializer_test.rb
+++ test/paradox_mod_file_serializer_test.rb
@@ -24,4 +24,70 @@ if = {
 }
 EOF
   end
+
+  def test_sample_3
+    assert_reserialization 3, <<EOF
+title = c_cagliari
+max_settlements = 4
+b_cagliari = castle
+b_ogliastra = city
+b_assemini = temple
+culture = italian
+religion = norse_pagan
+terrain = hills
+867.1.1 = {
+  b_cagliari = ca_shipyard_1
+}
+1066.1.1 = {
+  trade_post = b_caetani
+}
+1285.1.1 = {
+  trade_post = b_grimaldi
+}
+EOF
+  end
+
+  def test_sample_4
+    assert_reserialization 4, <<EOF
+christian = {
+  defender_of_faith = yes
+  catholic = {
+    color = {
+      0.8
+      0.8
+      0
+    }
+    icon = 1
+    allowed_conversion = {
+      reformed
+      protestant
+    }
+    country = {
+      tolerance_own = 1
+      tolerance_heretic = -1
+    }
+    on_convert = {
+      change_religion = catholic
+      add_prestige = -100
+      remove_country_modifier = the_test_act
+      remove_country_modifier = superintendent_office
+      remove_country_modifier = the_popery_act
+      add_country_modifier = {
+        name = conversion_zeal
+        duration = 3650
+      }
+    }
+    heretic = {
+      BOGOMILIST
+      WALDENSIAN
+      FRATICELLI
+      HUSSITE
+      LOLLARD
+      SOCINIAN
+    }
+    papacy = yes
+  }
+}
+EOF
+  end
 end
