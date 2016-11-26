dbd3165384171da297aae0086fea95b9bc8bae04
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-11-26 19:53:57 +0000

    Attempted ET 1.19.1 build (untested)

--- fun_and_balance/mods/fun_and_balance_et.rb
+++ fun_and_balance/mods/fun_and_balance_et.rb
@@ -14,15 +14,13 @@ class FunAndBalanceETGameModification < FunAndBalanceCommonGameModification
 
     soft_patch_defines_lua!([
       ["NAI.DIPLOMATIC_INTEREST_DISTANCE", 150, 200],
-      ["NAI.FORT_MAINTENANCE_CHEAT", 1, 0],
+      ["NAI.FORT_MAINTENANCE_CHEAT", 0, 0],
       ["NAI.PEACE_TIME_EARLY_FACTOR", 0.75, 0.50],
       ["NAI.PEACE_WAR_EXHAUSTION_FACTOR", 1.0, 2.0],
-      ["NCountry.ADVISOR_COST_INCREASE_PER_YEAR", 0.005,  0.0005],
-      ["NCountry.CULTURE_GAIN_THRESHOLD", "0.20", "0.10"],
-      ["NCountry.LIBERTY_DESIRE_GREAT_POWER_VASSAL", 50, 15],
+      ["NCountry.CULTURE_GAIN_THRESHOLD", "0.20", "0.10"],  # This goes away in 1.18 anyway
       ["NCountry.LIBERTY_DESIRE_HISTORICAL_FRIEND", -50, -30],
       ["NCountry.LIBERTY_DESIRE_HISTORICAL_RIVAL", 50, 30],
-      ["NCountry.LIBERTY_DESIRE_LARGE_VASSAL", 25, 15],
+      ["NCountry.LIBERTY_DESIRE_VASSAL_DEVELOPMENT_RATIO", 0.25, 0.10],
       ["NCountry.MAX_IDEA_GROUPS_FROM_SAME_CATEGORY", 0.5, 1.0],
       ["NCountry.POLICY_COST", 1, 0],
       ["NCountry.PROTECTORATE_LOWER_BOUND", 0.5, 5.0],
@@ -34,7 +32,6 @@ class FunAndBalanceETGameModification < FunAndBalanceCommonGameModification
       ["NCountry.RANDOM_LUCKY_SLOW_TECH_PENALTY", 0.33, 1],
       ["NCountry.RANDOM_LUCKY_TECH_WEIGHT", 1, 0],
       ["NCountry.TECH_AHEAD_OF_TIME", 0.1, 0.05],
-      ["NCountry.WESTERN_POWER_TICK_REDUCTION_FACTOR", 100, 1000000],
       ["NDiplomacy.ANNEX_DIP_COST_PER_DEVELOPMENT", 8, 4],
       ["NDiplomacy.INTEGRATE_VASSAL_MIN_YEARS", 10, 20],
       ["NDiplomacy.MONTHS_BEFORE_TOTAL_OCCUPATION", 60, 24],
