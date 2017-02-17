8a1ca27b0bd9ae76801882210b456413ef498b71
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-02-17 13:56:05 +0000

    Realm/province tech scripts

--- analysis_ck2/ck2_analyses.rb
+++ analysis_ck2/ck2_analyses.rb
@@ -24,6 +24,54 @@ module Ck2Analyses
     end
   end
 
+  def province_techs
+    @province_techs ||= begin
+      province_techs = []
+      @data["provinces"].each do |id, province|
+        # Ocean provinces now have flags (like disease)
+        next if province == []
+        name = province["name"]
+        next unless name
+        total_tech = province["technology"]["tech_levels"].inject(&:+).round(3)
+        province_techs << {
+          id: id,
+          tech: total_tech,
+          name: name,
+        }
+      end
+      province_techs
+    end
+  end
+
+  def realm_techs
+    @realm_techs ||= begin
+      parts = {}
+      province_techs.each do |info|
+        province_realms = realms(info[:id]).map{|realm| realm_name(realm)}
+        province_realms.each do |realm|
+          parts[realm] ||= []
+          parts[realm] << info[:tech]
+        end
+      end
+      parts.map{|realm, xx|
+        {
+          name: realm,
+          tech: (xx.inject(&:+) / xx.size).round(2),
+          size: xx.size,
+        }
+      }
+    end
+  end
+
+  def realms(id)
+    province = @data["provinces"][id]
+    province_titles = valid_titles & province.keys
+    top_realms = province_titles.map do |title|
+      top_realm_by_title(title)
+    end.uniq
+    top_realms.sort
+  end
+
   def player_id
     @data["player"]["id"]
   end
