c732ecd4b13b7b53441c75aab38cc20d211a9b49
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-05-25 23:02:42 +0100

    Deal with a bit more fake yaml (Kaiserreich has so much faily stuff), nowhere near getting all of it

--- lib/fake_yaml.rb
+++ lib/fake_yaml.rb
@@ -7,8 +7,13 @@ class FakeYaml
       path = Pathname(path)
       data = path.read
       data = data.gsub(/\uFEFF/, "") # strip UTF8 BOM
-      data = data.gsub(/^ \S+?:\K\d(?=[ \t])/){ "" } # strip version codes
-      data = data.gsub(/^ \S+?:[ \t]"\K(.*)(?="$)/){ fix_quotes($1) }
+      return if data !~ /\S/ # mod override
+      data = data.gsub(/^ *\S+?:\K\d(?=[ \t])/){ "" } # strip version codes
+      data = data.gsub(/^ *\S+?:[ \t]"\K(.*)(?="$)/){ fix_quotes($1) }
+      data = data.gsub(/\r/, "")
+      data = data.gsub(/\n+/, "\n")
+      data = data.gsub(/^\s*/, " ").sub(/\A\s*/, "")
+
       YAML.load(data)["l_english"].tap do |parsed|
         raise "No Engish localization data in `#{path}'" unless parsed
       end
