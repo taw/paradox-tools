2f3e29751c5c285b55eeb802383fff3afebc176e
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-11-19 11:31:23 +0000

    Accidental space removed

--- fun_and_balance/mods/disable_normal_or_historical_check.rb
+++ fun_and_balance/mods/disable_normal_or_historical_check.rb
@@ -3,7 +3,7 @@ require_relative "base"
 class DisableNormalOrHistoricalCheck < EU4GameModification
   def apply!
     patch_files!("**/*.txt", reencode: true) do |content|
-      next content unless content =~ /normal_or_historical_nations/                
+      next content unless content =~ /normal_or_historical_nations/
       content.gsub("normal_or_historical_nations", "always")
     end
   end
