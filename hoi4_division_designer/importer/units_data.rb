af95b5d507799456f314008e12d0f941cf7bf95d
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-07-14 19:12:58 +0200

    Moved UnitType logic from data class into importer

--- hoi4_division_designer/importer/units_data.rb
+++ hoi4_division_designer/importer/units_data.rb
@@ -11,26 +11,60 @@ class UnitsData
     end
   end
 
+  def default_values
+    {
+      "air_attack" => 0,
+      "breakthrough" => 0,
+      "casualty_trickleback" => 0,
+      "defense" => 0,
+      "entrenchment" => 0,
+      "equipment_capture_factor" => 0,
+      "experience_loss_factor" => 0,
+      "hard_attack" => 0,
+      "initiative" => 0,
+      "maximum_speed" => 0,
+      "recon" => 0,
+      "reliability_factor" => 0,
+      "soft_attack" => 0,
+      "supply_consumption_factor" => 0,
+      "suppression" => 0,
+      "supply_use" => 0,
+    }
+  end
+
+  def terrain_types
+    ["forest", "marsh", "hills", "urban", "mountain", "jungle", "amphibious", "river", "fort", "plains", "desert"]
+  end
+
+  # Some awkward names changed
+  def renamed
+    {
+      "need" => "equipment",
+      "supply_consumption" => "supply_use",
+      "max_organisation" => "org",
+      "max_strength" => "hp",
+      "default_morale" => "recovery_rate",
+    }
+  end
+
   def data
     result = {}
     each_unit do |name, unit|
       raise "Duplicate name: #{name}" if result[name]
       bonuses = {}
-      unit_data = {}
+      unit_data = default_values
       unit.each do |key, value|
         case key
-        when "forest", "marsh", "hills", "urban", "mountain",
-             "jungle", "amphibious", "river", "fort", "plains",
-             "desert"
+        when *terrain_types
           bonuses[key] = value
         when "priority", "ai_priority", "sprite", "map_icon_category"
           # Skip
         else
-          unit_data[key] = value
+          unit_data[renamed[key] || key] = value
         end
       end
 
-      unit_data["bonuses"] = bonuses
+      unit_data["bonuses"] = jsonify(bonuses)
       result[name] = unit_data
     end
     result
--- hoi4_division_designer/lib/unit_type.rb
+++ hoi4_division_designer/lib/unit_type.rb
@@ -1,136 +1,25 @@
-class UnitType
-  attr_reader :key
-
+class UnitType < OpenStruct
   def initialize(key, stats)
-    @key = key
-    @stats = stats
+    super(stats.merge("key" => key))
   end
 
-  def combat_width
-    @stats["combat_width"]
-  end
-
-  def manpower
-    @stats["manpower"]
-  end
-
-  def training_time
-    @stats["training_time"]
-  end
-
-  def suppression
-    @stats["suppression"] || 0
-  end
-
-  def hp
-    @stats["max_strength"]
-  end
-
-  def org
-    @stats["max_organisation"]
-  end
-
-  def weight
-    @stats["weight"]
-  end
-
-  def recovery_rate
-    @stats["default_morale"]
-  end
-
-  def equipment
-    @stats["need"]
-  end
-
-  def supply_use
-    @stats["supply_consumption"] || 0
-  end
-
-  def supply_consumption_factor
-    @stats["supply_consumption_factor"] || 0
-  end
-
-  def maximum_speed
-    @stats["maximum_speed"] || 0
-  end
-
-  def bonuses
-    @stats["bonuses"]
-  end
-
-  def special_forces?
-    @stats["special_forces"]
-  end
-
-  def soft_attack
-    @stats["soft_attack"] || 0
-  end
-
-  def hard_attack
-    @stats["hard_attack"] || 0
-  end
-
-  def air_attack
-    @stats["air_attack"] || 0
-  end
-
-  def breakthrough
-    @stats["breakthrough"] || 0
-  end
-
-  def defense
-    @stats["defense"] || 0
-  end
-
-  def can_be_parachuted?
-    @stats["can_be_parachuted"]
-  end
-
-  def entrenchment
-    @stats["entrenchment"] || 0
-  end
-
-  def recon
-    @stats["recon"] || 0
-  end
-
-  def active?
-    !!@stats["active"]
-  end
-
-  def categories
-    @stats["categories"] || []
+  def inspect
+    "Equipment<#{key}>"
   end
 
   def frontline?
     combat_width != 0
   end
 
-  def reliability_factor
-    @stats["reliability_factor"] || 0
+  def special_forces?
+    !!special_forces
   end
 
-  def casualty_trickleback
-    @stats["casualty_trickleback"] || 0
+  def can_be_parachuted?
+    !!can_be_parachuted
   end
 
-  def experience_loss_factor
-    @stats["experience_loss_factor"] || 0
-  end
-
-  def initiative
-    @stats["initiative"] || 0
-  end
-
-  def equipment_capture_factor
-    @stats["equipment_capture_factor"] || 0
-  end
-
-  def name
-    @stats["name"]
-  end
-
-  def inspect
-    "UnitType<#{@key}>"
+  def active?
+    !!active
   end
 end
